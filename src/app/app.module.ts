8df830bcf0ced3784bfb4e34bb609e37e6a3db8f
fgygh <fgygh5804@gmail.com>
2020-02-12 16:44:25 +0000

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AmplifyAngularModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
+  providers: [
+    AmplifyService,
+  ]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
